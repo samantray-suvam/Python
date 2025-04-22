aa0a68a840ffb94871407ba46adc053f397199a5
Suvam Samantray <suvam.workforce@gmail.com>
2025-04-22 14:55:02 +0530

    minor practices

--- asd.py
+++ asd.py
@@ -1,4 +1,3 @@
-
 # num1=15
 # num2=30
 # t=0
@@ -9,3 +8,78 @@
 # print(t)
 
 
+# num=234321
+# r=""
+# for i in str(num):
+#     r=i+r
+# print(r)
+
+
+# n="raju"
+# for i in range(len(n) - 1):
+#     print(i)
+
+
+# b="parrot"
+# for i in range(len(b)):
+#     if b[i]=="r":
+#         print(b[:i])
+
+
+# for i in range(15, 19):
+#     print(i)
+
+
+# fur="Chair"
+# r=""
+# for i in range(len(fur)):
+#     r=r + (fur[i] * i)
+# print(r)
+
+
+# num=input("Enter a number: ")
+# sum=0
+# for i in num:
+#     sum=sum+int(i)
+
+# print(sum)
+
+
+# w="Paper"
+# r=""
+# let=len(w)
+# for i in range(let):
+#     r = r + w[let-1]
+# print(r)
+
+
+# product of n numbers
+# n=int(input("num = "))
+# pro=1
+# for i in range(n):
+#     num=int(input())
+#     pro=num*pro    
+# print("product = ",pro)
+
+
+# row=int(input())
+# col=int(input())
+# for i in range(row):
+#     print(col * "* ")
+
+
+# n=int(input())
+# pro=1
+# for i in range(n):
+#     num=int(input())
+#     pro=num*pro
+# print(pro)
+
+
+# n=int(input())
+# pro=1
+# for i in range(n):
+#     num=int(input())
+#     pro=num*pro
+    
+# print(pro)
