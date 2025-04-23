4c819fc2af48f33412ad612d085880c046007eed
Suvam Samantray <suvam.workforce@gmail.com>
2025-04-23 16:18:19 +0530

    regular updates

--- FOR_Loop_Day2.py
+++ FOR_Loop_Day2.py
@@ -13,7 +13,6 @@
 
 
 # n=int(input())
-
 # def print_tri(n):
 #     for i in range(1,n+1):
 #         print(i * "* ")
@@ -47,3 +46,74 @@
 #     print("+ " * col)
 
 
+# m=int(input())
+# n=int(input())
+# sum=0
+# for i in range(m,n+1):
+#     if i%2==0:
+#         sum+=i
+# print(sum)
+
+
+# m=int(input())
+# n=int(input())
+# pro=1
+# for i in range(m,n+1):
+#     if i%2==1:
+#         pro*=i
+# print(pro)
+
+
+# n=int(input())
+# fact=1
+# for i in range(n,0,-1):
+#     fact*=i
+# print(fact)
+
+
+# m=int(input())
+# n=int(input())
+# res=""
+# for i in range(m,n+1):
+#     if i%2==1:
+#         res=res+str(i)+" "
+# print(res)
+
+
+# m=int(input())
+# res=0
+# for i in range(1,m+1):
+#     if i%2==1:
+#         res+=i
+# print(res)
+
+
+# m=int(input())
+# n=int(input())
+# sum=0
+# for i in range(m,n+1):
+#     if i%2==1:
+#         sum+=i
+# print(sum)
+
+
+# sum=0
+# for i in range(1,11):
+#     num=int(input())
+#     sum+=num
+# print(sum)
+# print(sum/10)
+
+
+# n=int(input())
+# sum=0
+# for i in range(1,n+1):
+#     if i%2==0:
+#         sum+=i
+# print(sum)
+
+
+# m=int(input())
+# for i in range(1,m+1):
+#     if i%2==0:
+#         print(i)
