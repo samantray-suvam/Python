603a933f93629ddcf23097a4018dd7fee0a87ec8
Suvam Samantray <suvam.workforce@gmail.com>
2025-04-23 14:30:01 +0530

    another day, another set of code

---
+++ FOR_Loop_Day2.py
@@ -0,0 +1,49 @@
+# row=int(input())
+# col=int(input())
+# for i in range(1,row+1):
+#     print((str(i) + " ") * col)
+
+
+# n=int(input())
+# sum=0
+# for i in range(n):
+#     num=int(input())
+#     sum+=num
+# print(sum/n)
+
+
+# n=int(input())
+
+# def print_tri(n):
+#     for i in range(1,n+1):
+#         print(i * "* ")
+# print_tri(n)
+# print_tri(n)
+
+
+# m=int(input())
+# n=int(input())
+# pro=1
+# for i in range(m,n+1):
+#     pro=pro*i
+# print(pro)
+
+
+# m=int(input())
+# sum=0
+# for i in range(m+1):
+#     sum+=(i**2)
+# print(sum)
+
+
+# m=int(input())
+# for i in range(m,0,-1):
+#     print(i)
+
+
+# row=int(input())
+# col=int(input())
+# for i in range(1,row+1):
+#     print("+ " * col)
+
+
