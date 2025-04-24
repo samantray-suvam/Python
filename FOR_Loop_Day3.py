9bec5d91b664d4a4e88aa47a06e36357f5760328
Suvam Samantray <suvam.workforce@gmail.com>
2025-04-24 16:43:04 +0530

    yet coding again

--- FOR_Loop_Day3.py
+++ FOR_Loop_Day3.py
@@ -78,3 +78,57 @@
 #     if i%2==1:
 #         t=t+str(i)+" "
 # print(t)
+
+
+# m=int(input())
+# n=int(input())
+# prod=1
+# for i in range(m,n+1):
+#     if i%3==0:
+#         prod*=i
+# print(prod)
+
+
+# n=int(input())
+# k=int(input())
+# sum=0
+# for i in range(1,n+1):
+#     sum+=(i**k)
+# print(sum)
+
+
+# n=int(input())
+# c=0
+# for i in range(1,n+1):
+#     if (i%6==0) & (i%8==0):
+#         c+=1
+# print(c)
+
+
+# a=int(input())
+# for i in range(a,0,-1):
+#     if i==a:
+#         print("* " * i)
+#     else:
+#         print("+ " * i)
+
+
+# m=int(input())
+# def print_tri(m):
+#     for i in range(1,m+1):
+#         print("* " * i)
+
+# def print_invert(m):
+#     for i in range(m,0,-1):
+#         print("* " * i)
+        
+# print_tri(m)
+# print_invert(m)
+
+
+# m=int(input())
+# n=int(input())
+# prod=1  
+# for i in range(m,n+1):
+#     prod*=i
+# print(prod)
