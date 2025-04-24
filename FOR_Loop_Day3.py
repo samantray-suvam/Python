5cb73868d1976d077dc7c48bffa75602019dbdbb
Suvam Samantray <suvam.workforce@gmail.com>
2025-04-24 15:59:04 +0530

    yet another day of coding

---
+++ FOR_Loop_Day3.py
@@ -0,0 +1,63 @@
+# n=int(input())
+# t=int(input())
+# for i in range(1,n+1):
+#     if i%t==0:
+#         print(i)
+
+
+# m=int(input())
+# for i in range(m):
+#     print("* " * (m-i))
+
+
+# m=int(input())
+# c=0
+# for i in range(1,m+1):
+#     if (i%2==0) & (i%3==0):
+#         c+=1
+# print(c)
+
+
+# a=input()
+# for i in a:
+#     if i=="a" or i=="z":
+#         print(i)
+
+
+# m=int(input())
+# n=int(input())
+# for i in range(n,m-1,-1):
+#     print(i)
+
+
+# a=int(input())
+# for i in range(1,a+1):
+#     if (i%2==0) & (i%3==0):
+#         print(i)
+
+
+# n=int(input())
+# for i in range(n,0,-1):
+#     print(i)
+
+
+# n=int(input())
+# m=int(input())
+# c=0
+# for i in range(1,n+1):
+#     if i%m==0:
+#         c+=1
+# print(c)
+
+
+# p=int(input())
+# for i in range(p):
+#     print((str(p-i) + " ") * (p-i))
+
+
+# a=input()
+# s=""
+# for i in range(len(a)):
+#     print(a[len(a)-(i+1)])
+
+
