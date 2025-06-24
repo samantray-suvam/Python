55d75fb2d38da9f682bf0319102ec5e8cf494ad7
Suvam Samantray <suvam.workforce@gmail.com>
2025-06-24 13:24:42 +0530

    g

--- FOR_Loop_Day6.py
+++ FOR_Loop_Day6.py
@@ -12,25 +12,33 @@
 # print(sum)
 
 
-n = int(input("Enter the number of rows: "))
-for i in range(1, n + 1):
-    spaces = n - i
-    stars = i
-    row = " " * spaces + "*" * stars
-    print(row)
+# n = int(input("Enter the number of rows: "))
+# for i in range(1, n + 1):
+#     spaces = n - i
+#     stars = i
+#     row = " " * spaces + "*" * stars
+#     print(row)
 
 
-    n=int(input())
-for i in range (1, n+1):
-    space = 2*(n-i)
-    star = i 
-    row = (" ")*space + ("* ")*star
-    print(row)
+#     n=int(input())
+# for i in range (1, n+1):
+#     space = 2*(n-i)
+#     star = i 
+#     row = (" ")*space + ("* ")*star
+#     print(row)
 
 
-    n=int(input())
+#     n=int(input())
+# for i in range(1, n+1):
+#     space = i-1
+#     num = n-space
+#     row = " "*space + str(num)*num
+#     print(row)
+
+
+n=int(input())
 for i in range(1, n+1):
-    space = i-1
-    num = n-space
-    row = " "*space + str(num)*num
+    space = (" ")*(i-1)
+    num = (n+1)-i
+    row = space + (str(num)+" ")*num
     print(row)
