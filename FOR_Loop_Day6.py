7b853861683c611e4492f97c2b84a932573e5953
Suvam Samantray <suvam.workforce@gmail.com>
2025-06-24 12:29:32 +0530

    c

--- FOR_Loop_Day6.py
+++ FOR_Loop_Day6.py
@@ -1,12 +1,20 @@
-x=int(input())
-n=int(input())
-sum=0
+# x=int(input())
+# n=int(input())
+# sum=0
 
-for i in range(1, n+1):
-    power = 2 * i 
-    term = x ** power
+# for i in range(1, n+1):
+#     power = 2 * i 
+#     term = x ** power
     
-    if i%2==0:
-        term = -term
-    sum+=term
-print(sum)
+#     if i%2==0:
+#         term = -term
+#     sum+=term
+# print(sum)
+
+
+n = int(input("Enter the number of rows: "))
+for i in range(1, n + 1):
+    spaces = n - i
+    stars = i
+    row = " " * spaces + "*" * stars
+    print(row)
