c4ce184556a6a825c9af13096b7f4769db4367ba
Suvam Samantray <suvam.workforce@gmail.com>
2025-06-25 10:34:34 +0530

    adad

--- FOR_Loop_Day7.py
+++ FOR_Loop_Day7.py
@@ -8,3 +8,15 @@ for i in range(1, n):
     star = ("* ")*i
     space = (" ")*(n-i)
     print(star + space*2 + star)
+
+
+n=int(input())
+for i in range(1, n+1):
+    star = ("* ")*i
+    space = (" ")*2*(n-i)
+    print(star + space*2 + star)
+
+for i in range(1, n):
+    star = ("* ")*(n-i)
+    space = (" ")*2*i 
+    print(star + space*2 + star)
